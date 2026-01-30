5859e3ddcda0388f699bd96c9cda4afc73eab86c
Isadora <rm84246@fiap.com.br>
2026-01-29 21:20:49 -0300

    CSVRepository para salvar dados de Weather

---
+++ infrastructure/csv_repository.py
@@ -0,0 +1,23 @@
+import csv
+import os
+
+class CSVRepository:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def save(self, weather_list):
+        file_exists = os.path.isfile(self.filename)
+
+        with open(self.filename, mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+
+            if not file_exists:
+                writer.writerow(["cidade", "temperatura_c", "condicao", "fonte"])
+
+            for weather in weather_list:
+                writer.writerow([
+                    weather.city,
+                    weather.temperature_c,
+                    weather.condition,
+                    weather.source
+                ])
